ebf3380aa92b184bd63c8075c2a35aad44e8aa55
endabelyu <endabelyuproject@gmail.com>
2026-04-28 05:51:55 +0700

    branding: switch sidebar logo to pkgxfl variant

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -26,7 +26,7 @@ export function Sidebar() {
       {/* Brand */}
       <div className="p-8 pb-4">
         <div className="flex items-center justify-start">
-          <img src="/Gemini_Generated_Image_cbonnwcbonnwcbon-removebg-preview.png" className="h-28 w-auto object-contain -ml-4 -mt-4" alt="Mili Logo" />
+          <img src="/Gemini_Generated_Image_pkgxflpkgxflpkgx-removebg-preview.png" className="h-28 w-auto object-contain -ml-4 -mt-4" alt="Mili Logo" />
         </div>
       </div>
 
